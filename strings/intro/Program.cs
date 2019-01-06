e7b4f149da357ced930a8cd753fcec0515c78a31
Kiril Stanoev <kiril.stanoev@gmail.com>
2019-01-07 00:02:02 +0200

    strings

---
+++ strings/intro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace intro {
+    class Program {
+        static void Main (string[] args) {
+            string str = "Hello World";
+            char[] chr = str.ToCharArray ();
+
+            Console.Write ("string: ");
+            Console.WriteLine (str);
+            Console.Write ("char[]: ");
+            Console.WriteLine (chr);
+
+            Console.Write ("string[0]: ");
+            Console.WriteLine (str[0]);
+            Console.Write ("char[0]: ");
+            Console.WriteLine (chr[0]);
+        }
+    }
+}
---
+++ strings/sum-numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sum_numbers {
+    class Program {
+        static void Main (string[] args) {
+            Console.Write ("input: ");
+            string[] inputs = Console.ReadLine ().Split (' ');
+            int sum = 0;
+            foreach (string text in inputs) {
+                int numeric_input;
+                if (int.TryParse (text, out numeric_input))
+                    sum += numeric_input;
+            }
+            Console.WriteLine ("output: " + sum);
+        }
+    }
+}
